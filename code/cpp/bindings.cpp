047bdba32ea75d0edb4002f1bbd2064971649c82
ben <benjamin.p.riviere@gmail.com>
2021-03-26 12:51:57 -0700

    cleaned!

--- code/cpp/bindings.cpp
+++ code/cpp/bindings.cpp
@@ -8,42 +8,52 @@
 #include "problems/example1.hpp"
 #include "solvers/puct.hpp"
 
-// // cpp code
-
-// global variables 
-std::random_device g_r;
-std::default_random_engine g_generator(g_r());
-
-void seed(size_t seed)
+class Problem_Wrapper
 {
-  g_generator = std::default_random_engine(seed);
-}
+    public: 
+        Example1 m_problem; 
 
-// helper 
-class Settings
+    // Problem_Wrapper(
+    //     std::string name,
+    // ){
+    //     // if (str1.compare(str2) != 0){
+    //     //     m_problem = 
+    //     // }
+    // }
+};
+
+
+class PUCT_Wrapper
 {
-public:
-  Settings()
-    : num_nodes(1000)
-    , search_depth(10)
-    , C_exp(1.0f)
-    , alpha_exp(0.25f)
-    , C_pw(2.0f)
-    , alpha_pw(0.5f)
-    , beta_policy(0.0)
-    , beta_value(0.0)
-    , vis_on(false)
-  {
-  }
-    int num_nodes;
-    int search_depth;
-    float C_exp;
-    float alpha_exp;
-    float C_pw;
-    float alpha_pw;
-    float beta_policy;
-    float beta_value;
-    bool vis_on; 
+    public: 
+        PUCT m_puct; 
+        std::default_random_engine m_gen;
+        bool m_vis_on;
+
+    PUCT_Wrapper(
+        int num_nodes,
+        int search_depth,
+        float C_exp,
+        float alpha_exp,
+        float C_pw,
+        float alpha_pw,
+        float beta_policy,
+        float beta_value,
+        bool vis_on)
+        : m_puct(m_gen
+                ,num_nodes
+                ,search_depth
+                ,C_exp
+                ,alpha_exp
+                ,C_pw
+                ,alpha_pw
+                ,beta_policy
+                ,beta_value)
+        , m_vis_on(vis_on)
+        {
+            std::random_device dev;
+            std::default_random_engine m_gen(dev());
+        }
 };
 
 class Result
@@ -53,192 +63,44 @@ public:
     Eigen::MatrixXf tree;
 };
 
-// primary function 
 Result search(
-    const Eigen::Matrix<float,2,1> &state,
-    const Settings &settings,
-    const int curr_seed){
+    PUCT_Wrapper & w_puct,
+    Problem_Wrapper & w_problem,
+    Eigen::Matrix<float,2,1> & state)
+    {
+        Result result;
 
-    Example1 problem; 
-    Result result;
+        // check valid 
+        if (!w_problem.m_problem.is_valid(state)) {
+            std::cout << "initial state not valid" << std::endl;
+            return result; 
+        }
 
-    seed(curr_seed);
+        // search
+        auto root_node = w_puct.m_puct.search(w_problem.m_problem,state); 
 
-    // check valid 
-    if (!problem.is_valid(state)) {
-        std::cout << "initial state not valid" << std::endl;
-        return result; 
-    }
-
-    // search settings 
-    int num_nodes = settings.num_nodes;
-    int search_depth = settings.search_depth;
-    float C_exp = settings.C_exp;
-    float alpha_exp = settings.alpha_exp;
-    float C_pw = settings.C_pw;
-    float alpha_pw = settings.alpha_pw;
-    float beta_value = settings.beta_value;
-    float beta_policy = settings.beta_policy;
-
-    // search
-    PUCT puct(g_generator,num_nodes,search_depth,C_exp,alpha_exp,C_pw,alpha_pw,beta_policy,beta_value);
-    auto root_node = puct.search(problem,state); 
-
-    // result 
-    result.best_action = puct.most_visited(&root_node,0)->action_to_node;
-    if (settings.vis_on) {
-        result.tree = puct.export_tree(problem); 
-    }
-    
-    return result;
+        // result 
+        result.best_action = w_puct.m_puct.most_visited(&root_node,0)->action_to_node;
+        if (w_puct.m_vis_on) {
+            result.tree = w_puct.m_puct.export_tree(w_problem.m_problem); 
+        }
+        return result;
 }
 
 // python interface
 PYBIND11_MODULE(bindings, m) {
 
-    m.def("cpp_search", &search, "PUCT");
-    // m.def("search", &search2, "PUCT2");
-
-    pybind11::class_<Settings> (m, "Settings")
-        .def(pybind11::init())
-        .def_readwrite("num_nodes", &Settings::num_nodes)
-        .def_readwrite("search_depth", &Settings::search_depth)
-        .def_readwrite("C_exp", &Settings::C_exp)
-        .def_readwrite("alpha_exp", &Settings::alpha_exp)
-        .def_readwrite("C_pw", &Settings::C_pw)
-        .def_readwrite("alpha_pw", &Settings::alpha_pw)
-        .def_readwrite("beta_policy", &Settings::beta_policy)
-        .def_readwrite("beta_value", &Settings::beta_value)
-        .def_readwrite("vis_on", &Settings::vis_on);
+    m.def("search", &search, "PUCT");
 
     pybind11::class_<Result> (m, "Result")
         .def(pybind11::init())
         .def_readwrite("best_action", &Result::best_action)
         .def_readwrite("tree", &Result::tree);
-} 
 
-// cpp code
+    pybind11::class_<PUCT_Wrapper> (m, "PUCT_Wrapper")
+        .def(pybind11::init<int, int, float, float, float, float, float, float, bool>());
 
-// helper 
-// class PUCT_Wrapper
-// {
-//     public: 
-//         PUCT m_puct; 
-//         std::default_random_engine m_gen;
-    
-//     // void initialize(Settings & settings){
-//     void init(
-//         int num_nodes,
-//         int search_depth,
-//         float C_exp,
-//         float alpha_exp,
-//         float C_pw,
-//         float alpha_pw,
-//         float beta_policy,
-//         float beta_value,
-//         bool vis_on)
-//         {
-//             std::random_device dev;
-//             std::default_random_engine m_gen(dev());
-
-//             m_puct = PUCT(m_gen
-//                 ,num_nodes
-//                 ,search_depth
-//                 ,C_exp
-//                 ,alpha_exp
-//                 ,C_pw
-//                 ,alpha_pw
-//                 ,beta_policy
-//                 ,beta_value);
-//         }
-// };
-
-// class Settings
-// {
-//     public:
-//         Settings()
-//             : num_nodes(1000)
-//             , search_depth(10)
-//             , C_exp(1.0f)
-//             , alpha_exp(0.25f)
-//             , C_pw(2.0f)
-//             , alpha_pw(0.5f)
-//             , beta_policy(0.0)
-//             , beta_value(0.0)
-//             , vis_on(false)
-//     {
-//     }
-//         int num_nodes;
-//         int search_depth;
-//         float C_exp;
-//         float alpha_exp;
-//         float C_pw;
-//         float alpha_pw;
-//         float beta_policy;
-//         float beta_value;
-//         bool vis_on; 
-// };
-
-// class Result
-// {
-// public:
-//     Eigen::Matrix<float,2,1> best_action;
-//     Eigen::MatrixXf tree;
-// };
-
-// Result search(
-//     PUCT & puct,
-//     Example1 & problem,
-//     Eigen::Matrix<float,2,1> & state)
-//     {
-
-//     Result result;
-
-//     // check valid 
-//     if (!problem.is_valid(state)) {
-//         std::cout << "initial state not valid" << std::endl;
-//         return result; 
-//     }
-
-//     // search
-//     auto root_node = puct.search(problem,state); 
-
-//     // result 
-//     result.best_action = puct.most_visited(&root_node,0)->action_to_node;
-//     if (true) {
-//         result.tree = puct.export_tree(problem); 
-//     }
-//     return result;
-// }
-
-// // python interface
-// PYBIND11_MODULE(bindings, m) {
-
-//     m.def("cpp_search", &search, "PUCT");
-
-//     pybind11::class_<Settings> (m, "Settings")
-//         .def(pybind11::init())
-//         .def_readwrite("num_nodes", &Settings::num_nodes)
-//         .def_readwrite("search_depth", &Settings::search_depth)
-//         .def_readwrite("C_exp", &Settings::C_exp)
-//         .def_readwrite("alpha_exp", &Settings::alpha_exp)
-//         .def_readwrite("C_pw", &Settings::C_pw)
-//         .def_readwrite("alpha_pw", &Settings::alpha_pw)
-//         .def_readwrite("beta_policy", &Settings::beta_policy)
-//         .def_readwrite("beta_value", &Settings::beta_value)
-//         .def_readwrite("vis_on", &Settings::vis_on);
-
-//     pybind11::class_<Result> (m, "Result")
-//         .def(pybind11::init())
-//         .def_readwrite("best_action", &Result::best_action)
-//         .def_readwrite("tree", &Result::tree);
-
-//     // pybind11::class_<PUCT> (m, "PUCT")
-//     //     .def(pybind11::init<int, int, float, float, float, float, float, float>());
-
-//     // pybind11::class_<Example1> (m, "Example1")
-//     //     .def(pybind11::init());
-// }
-
-
-
+    pybind11::class_<Problem_Wrapper> (m, "Problem_Wrapper")
+        // .def(pybind11::init<std::string>());
+        .def(pybind11::init());
+}
--- code/solvers/c_puct.py
+++ code/solvers/c_puct.py
@@ -1,13 +1,12 @@
 
 
-
 # standard
 import numpy as np 
 
 # custom
 import plotter 
 from solvers.solver import Solver 
-from cpp.build.bindings import cpp_search, Settings
+from cpp.build.bindings import search, PUCT_Wrapper, Result, Problem_Wrapper
 
 class C_PUCT(Solver):
 
@@ -37,6 +36,18 @@ class C_PUCT(Solver):
 		self.beta_policy = beta_policy 
 		self.beta_value = beta_value
 		self.vis_on = vis_on 
+		
+		self.w_puct = PUCT_Wrapper(
+			number_simulations,
+			search_depth,
+			C_pw,
+			alpha_pw,
+			C_exp,
+			alpha_exp,
+			beta_policy,
+			beta_value,
+			vis_on,
+		)
 
 	def policy(self,problem,root_state):
 		result = self.wrap_search(problem,root_state)
@@ -49,86 +60,13 @@ class C_PUCT(Solver):
 		if not problem.name == "example1":
 			print('problem {} not supported'.format(problem.name))
 			exit()
+		else: 
+			cpp_problem = Problem_Wrapper()
 
-		settings = Settings()
-		# settings.policy_oracle = self.policy_oracle # todo 
-		# settings.value_oracle = self.value_oracle 
-		settings.search_depth = self.search_depth 
-		settings.num_nodes = self.number_simulations 
-		settings.C_pw = self.C_pw 
-		settings.alpha_pw = self.alpha_pw 
-		settings.C_exp = self.C_exp 
-		settings.alpha_exp = self.alpha_exp 
-		settings.beta_policy = self.beta_policy 
-		settings.beta_value = self.beta_value
-		settings.vis_on = self.vis_on 
+		result = search(self.w_puct,cpp_problem,root_state)
 
-		# temp 
-		import random 
-		seed = random.randint(1,1000)
-
-		result = cpp_search(root_state,settings,seed)
-
-		if settings.vis_on: 
+		if self.vis_on: 
 			tree_state = result.tree 
-			plotter.plot_tree_state(problem,tree_state,zoom_on=False)
+			plotter.plot_tree_state(problem,tree_state,zoom_on=True)
 
 		return result
-
-# # standard
-# import numpy as np 
-
-# # custom
-# import plotter 
-# from solvers.solver import Solver 
-# from cpp.build.bindings import cpp_search, Settings, PUCT_Wrapper
-
-# class C_PUCT(Solver):
-
-# 	def __init__(self,
-# 		policy_oracle=None,\
-# 		value_oracle=None,\
-# 		search_depth=10,\
-# 		number_simulations=1000,
-# 		C_pw=2.0,
-# 		alpha_pw=0.5,
-# 		C_exp=1.0,
-# 		alpha_exp=0.25,
-# 		beta_policy=0.,
-# 		beta_value=0.,
-# 		vis_on=False,
-# 		):
-# 		super(C_PUCT, self).__init__()
-
-# 		settings = Settings()
-# 		settings.search_depth = search_depth 
-# 		settings.num_nodes = number_simulations 
-# 		settings.C_pw = C_pw 
-# 		settings.alpha_pw = alpha_pw 
-# 		settings.C_exp = C_exp 
-# 		settings.alpha_exp = alpha_exp 
-# 		settings.beta_policy = beta_policy 
-# 		settings.beta_value = beta_value
-# 		settings.vis_on = vis_on 
-
-# 		self.puct_wrapper = PUCT_Wrapper(settings)
-
-# 	def policy(self,problem,root_state):
-# 		result = self.wrap_search(problem,root_state)
-# 		py_action = np.zeros((problem.action_dim,1))
-# 		py_action[:,0] = result.best_action
-# 		return py_action
-
-# 	def wrap_search(self,problem,root_state):
-
-# 		if not problem.name == "example1":
-# 			print('problem {} not supported'.format(problem.name))
-# 			exit()
-
-# 		result = self.puct_wrapper(root_state)
-
-# 		if settings.vis_on: 
-# 			tree_state = result.tree 
-# 			plotter.plot_tree_state(problem,tree_state,zoom_on=True)
-
-# 		return result
