76d04efd3c0a30638423da764efe3fc489e40cfe
ben <benjamin.p.riviere@gmail.com>
2021-04-24 10:12:51 -0700

    remove if condition for loading param

--- code/param.py
+++ code/param.py
@@ -6,25 +6,25 @@ class Param:
 	def __init__(self):
 
 		# names 
-		self.problem_name = "example1" # e.g. example1, example2, example3, ...
-		self.solver_name = "C_PUCT_V1" # e.g. Empty, DARE, PUCT_V0, C_PUCT_V0, PUCT_V1, ...
+		self.problem_name = "example2" # e.g. example1, example2, example3, ...
+		self.solver_name = "C_PUCT_V0" # e.g. Empty, DARE, PUCT_V0, C_PUCT_V0, PUCT_V1, ...
 
 		# settings
 		self.movie_on = False
 
 		# solver settings 
-		if self.solver_name in ["PUCT_V0","PUCT_V1","C_PUCT_V0","C_PUCT_V1"]:
-			self.number_simulations = 10
-			self.policy_oracle = None
-			self.value_oracle = None
-			self.search_depth = 10
-			self.C_pw = 2.0
-			self.alpha_pw = 0.5
-			self.C_exp = 1.0
-			self.alpha_exp = 0.25
-			self.beta_policy = 0.0
-			self.beta_value = 0.0
-			self.vis_on = False
+		# if self.solver_name in ["PUCT_V0","PUCT_V1","C_PUCT_V0","C_PUCT_V1"]:
+		self.number_simulations = 1000
+		self.policy_oracle = None
+		self.value_oracle = None
+		self.search_depth = 10
+		self.C_pw = 2.0
+		self.alpha_pw = 0.5
+		self.C_exp = 1.0
+		self.alpha_exp = 0.25
+		self.beta_policy = 0.0
+		self.beta_value = 0.0
+		self.vis_on = True
 
 	def to_dict(self):
 		return self.__dict__
