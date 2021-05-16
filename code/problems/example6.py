3a632d3409904a4950ea15da261b543976cf586b
ben <benjamin.p.riviere@gmail.com>
2021-05-16 10:33:51 -0700

    revert parameters example6

--- code/problems/example6.py
+++ code/problems/example6.py
@@ -17,9 +17,9 @@ class Example6(Problem):
 		super(Example6,self).__init__()
 
 		self.t0 = 0
-		self.tf = 100
-		self.dt = 0.2
-		self.r_max = 100
+		self.tf = 20
+		self.dt = 0.5
+		self.r_max = 10
 		self.r_min = -self.r_max
 		self.num_robots = 1
 		self.gamma = 0.999
